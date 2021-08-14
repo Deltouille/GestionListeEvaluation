2390c55ecee4d3eaf0afbd4123f6ba1047e6f628
Deltouille <alexandre.petit7676@gmail.com>
2021-08-14 22:39:25 +0200

    ajout compteur

---
+++ src/Twig/CountStatutExtension.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Twig;
+
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFilter;
+use Twig\TwigFunction;
+
+class CountStatutExtension extends AbstractExtension
+{
+    public function getFilters(): array
+    {
+            // If your filter generates SAFE HTML, you should add a third
+            // parameter: ['is_safe' => ['html']]
+            // Reference: https://twig.symfony.com/doc/2.x/advanced.html#automatic-escaping
+            return [
+                new TwigFilter('filter_name', [$this, 'doSomething']),
+            ];
+    }
+
+    public function getFunctions(): array
+    {
+        
+
+        return [
+            // If your filter generates SAFE HTML, you should add a third
+            // parameter: ['is_safe' => ['html']]
+            // Reference: https://twig.symfony.com/doc/2.x/advanced.html#automatic-escaping
+            new TwigFunction('getStatutToDo', [$this, 'getStatutToDo']),
+            new TwigFunction('getStatutEnCours', [$this, 'getStatutEnCours']),
+            new TwigFunction('getStatutTermine', [$this, 'getStatutTermine']),
+        ];
+    }
+
+    public function getStatutToDo($tache){
+        $toDo = 0;
+        foreach($tache as $laTache){
+            if($laTache->getStatut() == 'A Faire'){
+                $toDo += 1;
+            }
+        }
+        return $toDo;
+    }
+
+    public function getStatutEnCours($tache){
+        $enCours = 0;
+        foreach($tache as $laTache){
+            if($laTache->getStatut() == 'En Cours'){
+                $enCours += 1;
+            }
+        }
+        return $enCours;
+    }
+
+    public function getStatutTermine($tache){
+        $termine = 0;
+        foreach($tache as $laTache){
+            if($laTache->getStatut() == 'Terminé'){
+                $termine += 1;
+            }
+        }
+        return $termine;
+    }
+}
